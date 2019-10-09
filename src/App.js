3f387c3491c38d3a4d0e1b5bdf8f20150be34514
eneax <19818937+eneax@users.noreply.github.com>
2019-10-09 18:43:55 +0200

    Use Switch and Route in App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from './components/header'
+
+
+class App extends React.Component {
+  render() {
+    return (    
+      <Switch>
+        <Route exact path='/' component={Header} />
+      </Switch>
+    )
+  }
 }
 
-export default App;
+export default App
