813d5b98ca30c5d862891e2913bbdc8eb0bf1cf3
eneax <19818937+eneax@users.noreply.github.com>
2019-10-10 20:49:33 +0200

    Style single photo wrapper

---
+++ src/components/styles/singleStyles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+
+export const SinglePhotoWrapper = styled.div`
+  background: white;
+  display: flex;
+  max-width: 900px;
+  margin: 0 auto 2rem;
+
+  flex-basis: calc(33.333% - 4rem);
+  flex-grow: 1;
+  flex-shrink: 0;
+  padding: 2rem;
+  border: 1px solid lightgray;
+  background: white;
+  box-shadow: 0 0 0 5px rgba(0,0,0,0.03);
+  position: relative;
+`
