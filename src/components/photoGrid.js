665c06d00f6f99a7ecaf1b7d85dfc67052bf3f94
eneax <19818937+eneax@users.noreply.github.com>
2019-10-10 20:50:04 +0200

    Create PhotoGrid component

---
+++ src/components/photoGrid.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { PhotoGridWrapper } from './styles/photoGridStyles'
+
+
+class PhotoGrid extends React.Component {
+  render() {
+    return (
+      <PhotoGridWrapper>
+        I'm the photo grid Component
+      </PhotoGridWrapper>
+    )
+  }
+}
+
+export default PhotoGrid
