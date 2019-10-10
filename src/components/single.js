1d5fda89d245e5578a0e90b3638e491c49515452
eneax <19818937+eneax@users.noreply.github.com>
2019-10-10 20:48:48 +0200

    Create Single component

---
+++ src/components/single.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { SinglePhotoWrapper } from './styles/singleStyles'
+
+
+class Single extends React.Component {
+  render() {
+    return (
+      <SinglePhotoWrapper>
+        I'm the single Component
+      </SinglePhotoWrapper>
+    )
+  }
+}
+
+export default Single
