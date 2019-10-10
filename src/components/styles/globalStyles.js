a4cd5efe0754b34d2c417f2fede38bd40b0020c7
eneax <19818937+eneax@users.noreply.github.com>
2019-10-10 16:55:02 +0200

    Delete index.css

--- src/components/styles/globalStyles.js
+++ src/components/styles/globalStyles.js
@@ -31,6 +31,21 @@ export const GlobalStyle = createGlobalStyle`
     font-family: sans-serif;
   }
 
+  body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+      "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
+      monospace;
+  }
+
+
   p {
     font-size: 1.6rem;
     line-height: 1.5;
