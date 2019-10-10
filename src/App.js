58457e3648529a44f45a252ac877f0563a035800
eneax <19818937+eneax@users.noreply.github.com>
2019-10-10 16:51:32 +0200

    Use GlobalStyle in App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 
+import { GlobalStyle } from './components/styles/globalStyles'
 import Header from './components/header'
 
 
 class App extends React.Component {
   render() {
-    return (    
-      <Switch>
-        <Route exact path='/' component={Header} />
-      </Switch>
+    return (
+      <>
+        <GlobalStyle />
+
+        <Switch>
+          <Route exact path='/' component={Header} />
+        </Switch>
+      </>
     )
   }
 }
